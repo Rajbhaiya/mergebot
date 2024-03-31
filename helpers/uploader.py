b2a6a459f35b6e000ac0a32db934d1736ef9b69f
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2024-03-31 12:44:35 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -1,5 +1,6 @@
-import asyncio
 import os
+import subprocess
+import asyncio
 import time
 
 from __init__ import LOGGER
@@ -9,7 +10,6 @@ from pyrogram import Client
 from pyrogram.types import CallbackQuery, Message
 
 from helpers.display_progress import Progress
-
 
 async def uploadVideo(
     c: Client,
@@ -22,7 +22,18 @@ async def uploadVideo(
     file_size,
     upload_mode: bool,
 ):
-    # Report your errors in telegram group (@yo_codes).
+    # Split video based on Config.IS_PREMIUM
+    max_size = 3.95 * 1024 * 1024 * 1024 if Config.IS_PREMIUM else 1.95 * 1024 * 1024 * 1024
+    if os.path.getsize(merged_video_path) > max_size or not upload_mode:
+        parts = split_video(merged_video_path, max_size)
+        for i, part_path in enumerate(parts, start=1):
+            part_name = f"part{i:03d}.mp4"
+            os.rename(part_path, os.path.join(os.path.dirname(part_path), part_name))
+            await upload_part(c, cb, part_path, width, height, duration, video_thumbnail, upload_mode)
+    else:
+        await upload_part(c, cb, merged_video_path, width, height, duration, video_thumbnail, upload_mode)
+
+async def upload_part(c, cb, part_path, width, height, duration, video_thumbnail, upload_mode):
     if Config.IS_PREMIUM:
         sent_ = None
         prog = Progress(cb.from_user.id, c, cb.message)
@@ -43,27 +54,6 @@ async def uploadVideo(
                         c_time,
                     ),
                 )
-            else:
-                c_time = time.time()
-                sent_: Message = await userBot.send_document(
-                    chat_id=int(LOGCHANNEL),
-                    document=merged_video_path,
-                    thumb=video_thumbnail,
-                    caption=f"`{merged_video_path.rsplit('/',1)[-1]}`\n\nMerged for: <a href='tg://user?id={cb.from_user.id}'>{cb.from_user.first_name}</a>",
-                    progress=prog.progress_for_pyrogram,
-                    progress_args=(
-                        f"Uploading: `{merged_video_path.rsplit('/',1)[-1]}`",
-                        c_time,
-                    ),
-                )
-            if sent_ is not None:
-                await c.copy_message(
-                    chat_id=cb.message.chat.id,
-                    from_chat_id=sent_.chat.id,
-                    message_id=sent_.id,
-                    caption=f"`{merged_video_path.rsplit('/',1)[-1]}`",
-                )
-                # await sent_.delete()
     else:
         try:
             sent_ = None
@@ -72,15 +62,15 @@ async def uploadVideo(
                 c_time = time.time()
                 sent_: Message = await c.send_video(
                     chat_id=cb.message.chat.id,
-                    video=merged_video_path,
+                    video=part_path,
                     height=height,
                     width=width,
                     duration=duration,
                     thumb=video_thumbnail,
-                    caption=f"`{merged_video_path.rsplit('/',1)[-1]}`",
+                    caption=f"`{part_path.rsplit('/',1)[-1]}`",
                     progress=prog.progress_for_pyrogram,
                     progress_args=(
-                        f"Uploading: `{merged_video_path.rsplit('/',1)[-1]}`",
+                        f"Uploading: `{part_path.rsplit('/',1)[-1]}`",
                         c_time,
                     ),
                 )
@@ -88,12 +78,12 @@ async def uploadVideo(
                 c_time = time.time()
                 sent_: Message = await c.send_document(
                     chat_id=cb.message.chat.id,
-                    document=merged_video_path,
+                    document=part_path,
                     thumb=video_thumbnail,
-                    caption=f"`{merged_video_path.rsplit('/',1)[-1]}`",
+                    caption=f"`{part_path.rsplit('/',1)[-1]}`",
                     progress=prog.progress_for_pyrogram,
                     progress_args=(
-                        f"Uploading: `{merged_video_path.rsplit('/',1)[-1]}`",
+                        f"Uploading: `{part_path.rsplit('/',1)[-1]}`",
                         c_time,
                     ),
                 )
@@ -108,6 +98,30 @@ async def uploadVideo(
                     caption=f"`{media.file_name}`\n\nMerged for: <a href='tg://user?id={cb.from_user.id}'>{cb.from_user.first_name}</a>",
                 )
 
+def split_video(input_file, max_size):
+    input_size = os.path.getsize(input_file)
+    if input_size <= max_size:
+        return [input_file]
+
+    num_parts = (input_size + max_size - 1) // max_size
+    part_size = (input_size + num_parts - 1) // num_parts
+
+    parts = []
+    for i in range(num_parts):
+        part_file = f"{input_file}_{i+1}.mp4"
+        start_byte = i * part_size
+        cmd = [
+            "ffmpeg", "-i", input_file, "-c", "copy",
+            "-avoid_negative_ts", "make_zero", "-start_at_zero",
+            "-copyts", "-y", "-nostats",
+            "-loglevel", "0",
+            "-ss", str(start_byte), "-fs", str(part_size),
+            part_file
+        ]
+        subprocess.run(cmd)
+        parts.append(part_file)
+
+    return parts
 
 async def uploadFiles(
     c: Client,
