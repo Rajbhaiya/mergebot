82a0631905eea4d20e5389fe96b0943b3f730bd1
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2024-07-17 16:35:27 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -37,6 +37,7 @@ async def uploadVideo(
                 if metadata.has("duration"):
                     duration = metadata.get("duration").seconds
                 await upload_part(c, cb, part_path, width, height, duration, video_thumbnail, upload_mode)
+                os.remove(part_path)
             except Exception as e:
                 LOGGER.error(f"Failed to upload part: {e}")
     else:
@@ -130,7 +131,6 @@ async def upload_part(c, cb, part_path, width, height, duration, video_thumbnail
                     chat_id=int(LOGCHANNEL),
                     caption=f"`{media.file_name}`\n\nMerged for: <a href='tg://user?id={cb.from_user.id}'>{cb.from_user.first_name}</a>",
                 )
-    os.remove(part_path)
                 
 def split_video(video_path, max_size_bytes):
     """
